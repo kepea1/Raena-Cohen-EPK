f51e4222f6c9b5ac1965260179da6f37f7db6db9
K-velvet-P <123256886+K-velvet-P@users.noreply.github.com>
2024-01-19 15:47:17 +0000

    bio update

--- my-app/app/bio/page.js
+++ my-app/app/bio/page.js
@@ -9,41 +9,47 @@ export default function Bio() {
       </div>
       <div className={styles.content}>
         <div className="animate__animated animate__fadeIn">
-        <main className={styles.text}>
-          <p>
-            Soulful dreamscapes coloured with melodies &amp; poetry is Raena
-            Cohen&apos;s exciting new sound. She is currently recording her
-            debut EP, introducing her infectious vocal hooks with live drums and
-            jazz influenced sound. Recording with a full live band including
-            improvisational live takes as well as fresh new songs this EP
-            <b> &apos;Higher Planes&apos;</b> is one to look for in 2024.{" "}
-          </p>
-          <br />
-          <p>
-            Born in the vibrant town of Brighton, Raena trained in musical
-            theatre from a young age. Music has always been an essential part of
-            her life, growing up around the free party/rave scene, Raena was
-            freestyling over DNB and Jungle from a young age and started out
-            making liquid DNB records. Raena&apos;s true musical passion is
-            soul, jazz and hiphop, with influences such as Erykah Badu, Amy
-            Winehouse, Hiatus Kaiyote and Nina Simone. Her independent artistry
-            recently reborn out of the vibrant jazz scene in Brighton, led her
-            to work with other jazz musicians to record the demos Raena produced
-            in her bedroom.
-          </p>
-          <br />
-          <p>
-            She has performed in many grassroots venues mainly in Brighton and
-            London and festivals including Boomtown. In 2024, Raena is looking
-            to take her full band on tour around UK cities and festivals. After
-            a hiatus from making music, &apos;Higher Planes&apos; is the
-            revisiting of memories & detangling trauma, questioning current
-            issues and finding solace in melodic flows. With her strong and
-            touching jazz vocals Raena always focuses on the real and with this
-            hopes to touch the listener giving a moment of space, soothing the
-            soul and igniting the inspiration of the people.
-          </p>
-        </main>
+          <main className={styles.text}>
+            <p>
+              Born in the vibrant town of Brighton, Raena trained in musical
+              theater from a young age. Music has always been an essential part
+              of her life, but her journey into music has not been conventional.
+              Growing up around the free festival scene, Raena was freestyling
+              over DNB and Jungle from a young age and started out making liquid
+              DNB records. However, Raena’s true musical passion is soul, jazz
+              and hip-hop with influences such as Erykah Badu, Amy Winehouse,
+              Souls of Mischief, Hiatus Kaiyote and Nina Simone.{" "}
+            </p>
+            <br></br>
+            <p>
+              Raena has released two singles <b>‘See The Light’</b> and{" "}
+              <b>‘Weighs Down’</b> both well received by supporters and local
+              radio stations. These songs are from a previous collab with
+              producer Os Price and have a sultry hip-hop feel with neo-soul
+              melodic vocals. Since the release of these singles, Raena has
+              spent a lot of time crafting and producing her sound with more
+              control over the entire creative process, leading to her latest
+              collection of demos. After spending time in the vibrant up and
+              coming jazz scene in Brighton, Reana was able to share her work
+              with a band of jazz musicians and together they are recording this
+              exciting new project entitled <b>‘Higher Planes’</b>. This project
+              is the introduction of her sound to the world and the industry.
+              With infectious vocal hooks, live drums and jazz influenced
+              instrumentals,<b> ‘Higher Planes’ </b>is the revisiting of
+              memories & detangling trauma, questioning current issues and
+              finding solace in melodic flows. Recording with a full live band
+              including improvisational live takes as well as fresh new songs
+              this EP is one to look for in 2024. Think Voodoo, Mummas Gun,
+              Frank.
+            </p>
+            <br></br>
+            <p>
+              As well as putting on her own spoken word and female led music
+              events, Raena has performed in many venues, supporting local
+              artists, at open mics, as well as hip-hop cyphers and jazz jams in
+              Brighton and London, also at festivals including Boomtown.
+            </p>
+          </main>
         </div>
       </div>
     </>
